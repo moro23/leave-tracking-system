6c2ad0cb95066dbf3c09b814d50192e7fc100f5e
moro23 <billhab66@gmail.com>
2026-04-11 09:10:49 +0000

    feat: enhance Employee class with leave balance management and validation

--- src/com/leavetracker/model/Employee.java
+++ src/com/leavetracker/model/Employee.java
@@ -1,9 +1,16 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+
 public class Employee {
     // lets create class attributes 
     private int empID; 
     private String empName; 
     private String empDept; 
     private String empEmail; 
+
+    // lets set the leave balance of an employee at the beginning of the year.
+    private int empLeaveBalance = 40 ; 
 
 
     // lets create the constructor 
@@ -23,17 +30,25 @@ public class Employee {
         return empName; 
     }
 
-    public String getempDept(){
+    public String getEmpDept(){
         return empDept;
     }
 
-    public String getempEmail(){
+    public String getEmpEmail(){
         return empEmail;
+    }
+
+    public int getEmpLeaveBalance(){
+        return empLeaveBalance; 
     }
 
     // lets create setters 
     public void setEmpName(String empName){
-        this.empName = empName;
+        if (empName.isEmpty()){
+            System.err.println("Employee name cannot be empty.");
+        }else{
+            this.empName = empName;
+        }
     }
 
     public void setEmpDept(String empDept){
@@ -41,8 +56,49 @@ public class Employee {
     }
 
     public void setEmpEmail(String empEmail){
-        this.empEmail = empEmail;
+        if (empEmail == null || empEmail.contains("@")){
+          System.out.println("Employee email must be a valid email address");
+        }else{
+            this.empEmail = empEmail;
+        }
+      
     }
+
+
+    public void setEmpLeaveBalance(int empLeaveBalance){
+        // lets check if the employees leave balance is alwasy create than o
+        if (empLeaveBalance >= 0){
+            this.empLeaveBalance = empLeaveBalance;
+        }else{
+            System.out.println("Employee does not have any remaining leave days.");
+        }
+       
+    }
+
+
+    // lets create a function to apply for a leave 
+    public void applyLeaveRequest(LeaveRequest request){
+
+        // lets get the start and end date of the leave
+        LocalDate starDate = LocalDate.parse(request.getStartDate()); 
+        LocalDate endDate = LocalDate.parse(request.getEndDate());
+        
+        // compute the actual leave days from the end and start date
+        int atualLeaveDays = (int) ChronoUnit.DAYS.between(starDate, endDate);
+
+        //check if the remaning leavedays is greater or eqaul to the actual leave days requestsed 
+        if (empLeaveBalance >= atualLeaveDays){
+            // substract the actualLeaveDays from the emp leave balance and return the new balance
+            empLeaveBalance -= actualLeaveDays; 
+            request.setStatus("Approved");
+            System.out.print("Your leave has been granted.");
+        }else{
+            request.setStatus("Denied");
+            System.out.println("You don't have any remaining leave days");
+        }
+
+    }
+
 
     public String toString(){
         return "EmpId: " + empID + " , Name: " + empName + " , Department: " + empDept + " , Email: " + empEmail;
